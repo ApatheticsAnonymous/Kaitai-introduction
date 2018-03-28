755f29795f741c51da677edeb3664e71d1bd0eab
0-0 <>
2018-03-28 01:34:34 -0400

    Building with kaitai runtime for animal_record example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
+#include <fstream>
 
+#include "animal_record.h"
 
 int main(int argc, char **argv)
 {
+	if(argc != 2)
+		exit(-1);
 
-	return 0
+	std::ifstream ifs(argv[1], std::ifstream::binary);
+	kaitai::kstream ks(&ifs);
+	animal_record_t animal = animal_record_t(&ks);
+
+	std::cout << "name:\t" << animal.name() << '\n';
+	std::cout << "weight:\t" << animal.weight() << '\n';
+
+	return 0;
 }
